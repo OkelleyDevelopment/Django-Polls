a83e8446892b369f810700327761a1c2ccd5dcb5
OkelleyDevelopment <okelleynp@gmail.com>
2021-06-22 20:55:04 -0400

    feat: Project is done

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,21 +2,35 @@ from django.shortcuts import render, get_object_or_404
 from django.template import loader
 from django.http import HttpResponse,  HttpResponseRedirect
 from django.urls import reverse
+from django.views import generic
+from django.utils import timezone
 
 from .models import Question
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {'latest_question_list': latest_question_list}
-    return render(request, 'polls/index.html', context)
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
+
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
